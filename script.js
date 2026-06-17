ac2a890e2b9c868390db127f333dfdcf24f47a39
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-06-17 10:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const strongEle = document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
-
+	strongEle.forEach(element =>{
+		element.style.color = "rgb(0,28.0)"
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
+	strongEle.forEach(element =>{
+		element.style.color = "rgb(0,0.0)"
+	})
     
 }
